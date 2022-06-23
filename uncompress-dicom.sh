d27b875e2f568b7930c29387e9eb6696ec259f1f
uoshvis <uoshvis@gmail.com>
2022-06-23 11:42:49 +0300

    add uncompress-dicom script

---
+++ uncompress-dicom.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+echo "RUN FROM the INSIDE UNCOMPRESSED DIR"
+
+echo "Starting DICOM uncompresser"
+
+# check if args exist
+
+if [ -z "$1" ]
+then
+    echo "Please provide: sh ./script.sh <inputDir> <outputDir>"
+    exit 0
+fi
+
+# check args
+
+if [ $# -eq 1 ]
+then
+    dirIn=$1
+    dirOut="outputDir"
+elif [ $# -eq 2 ]
+then
+    dirIn=$1
+    dirOut=$2
+else
+    echo "Too many args"
+    exit 0
+fi
+
+#Create output dir
+
+{ # try
+    mkdir $dirOut &&
+    echo "Output directory created: /${dirOut}"
+    #save your output
+} || { # catch
+    echo "Already exists: /${dirOut}"
+    exit
+    # save log for exception
+}
+
+# Convert files
+cd $dirIn
+
+for FILE in *.dcm;
+    do {
+        gdcmconv --raw $FILE $dirOut/$FILE &&
+        echo "Uncompressed: ${FILE}"
+    } ||
+    {
+        echo "Failed to Uncompress: ${FILE}"
+    }
+done
+
+echo "Finished"
